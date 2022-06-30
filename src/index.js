3dc89c89b6a5d5ad5c7511f171ee8524089495b3
MateuszBachor <mateuszbachor3@gmail.com>
2022-06-30 22:03:17 +0200

    adsa
    asdas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector("search-box")
+input.setAttribute("value")
+console.log(input)
+console.log("asds")
+// function fetchCountries (){ fetch("https://restcountries.com/v3.1/all")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+// }
