65f1b20a1504799fdbbe6fc5b39b2403c65b0bc5
MateuszBachor <92057655+MateuszBachor@users.noreply.github.com>
2022-07-02 16:04:35 +0000

    Deploying to gh-pages from @ MateuszBachor/goit-js-hw-10@10c86ae9a409f3dfb77c36fbdebab6e547f615bb 🚀

--- index.599aef95.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("search-box");e.setAttribute("value"),console.log(e),console.log("asds");
-//# sourceMappingURL=index.599aef95.js.map
--- index.cd15ee97.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("search-box");e.setAttribute("value"),console.log(e),console.log("asds")}();
-//# sourceMappingURL=index.cd15ee97.js.map
